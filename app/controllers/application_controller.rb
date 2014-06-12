2e9088c477505026dfb4741da3c3f89ba58ca4f2
Sergio Nunes <sbrnunes@gmail.com>
2014-06-12 01:58:30 +0100

    Added matching with labels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def assert_date(date, error_message)
+    begin
+      Date.parse(date)
+    rescue
+      raise InvalidStatementError, error_message
+    end
+  end
+
+  def assert_description(description, error_message)
+    raise InvalidStatementError, error_message if description.nil?
+  end
+
+  def is_not_nil(str)
+    true unless str.nil?
+  end
+
 end
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -14,16 +14,24 @@ class HomepageController < ApplicationController
   end
 
   def upload_file
-    return redirect_to :back, flash: {error: 'No file has been selected.'} if params['csv_file'].nil?
+    return redirect_to(:back, flash: {error: 'No file has been selected.'}) if params['csv_file'].nil?
 
-    uploaded_file = params['csv_file'].read
+    rows = parse(params['csv_file'])
+    consolidate(rows)
+    validate(rows)
 
-    rows = CSV.parse(uploaded_file, :headers => true, :header_converters => :symbol, :converters => :all, :col_sep => ';')
+    labels = find_all_labels
 
-    process_rows(rows)
-
-    #Select only the rows that have a credit or a debit and map them to a json object
-    json_data = rows.select { |row| is_not_nil(row[2]) || is_not_nil(row[3]) }.map { |row| row.to_hash }
+    json_data = rows.select {|row| !(row[2].nil? and row[3].nil?) }.map do |row|
+      {
+          :date => Date.parse(row[0]).strftime('%Y-%m-%d'),
+          :description => row[1],
+          :debit => row[2],
+          :credit => row[3],
+          :labels => labels.select { |label| label[:applied_to].any? { |applied_to| row[1].include? applied_to[:statement_fragment]} }.
+              map { |label| label[:name] }
+      }
+    end
 
     directory = 'public/data'
 
@@ -35,31 +43,42 @@ class HomepageController < ApplicationController
     # write the file
     File.open(path, 'wb') { |f| f.write(JSON.pretty_generate(json_data)) }
 
-    redirect_to :back, flash: {success: 'File has been uploaded successfuly'}
+    redirect_to(:back, flash: {success: 'File has been uploaded successfuly'})
   end
 
-  def process_rows(rows)
+  def parse(csv_file)
+    CSV.parse(csv_file.read, :headers => true, :header_converters => :symbol, :converters => :all, :col_sep => ';')
+  end
+
+  # Iterates over each row, populates all the missing dates and merges related rows (ATM)
+  def consolidate(rows)
     rows.each_cons(2) do |curr_row, next_row|
-      assert curr_row
-      curr_row[0] = Date.parse(curr_row[0]).strftime('%Y-%m-%d')
-      next_row[0] = curr_row[0] if next_row[0].nil? #every statement must contain a valid date
+      next_row[0] = curr_row[0] if next_row[0].nil? unless curr_row[0].nil?
+      curr_row[1] += ' ' + next_row[1] if next_row[2].nil? and next_row[3].nil? unless curr_row[2].nil? and curr_row[3].nil?
     end
   end
 
-  def assert(row)
-    begin
-      assert_date row[0]
-    rescue
-      raise InvalidStatementError, 'The following statement does not contain a valid date: ' + row.to_hash.to_s
+  # Asserts that each row contains all the required data
+  def validate(rows)
+    rows.each do |row|
+      assert_date(row[0], 'The following statement does not contain a valid date: ' + row.to_hash.to_s)
+      assert_description(row[1], 'The following statement does not contain a description field: ' + row.to_hash.to_s)
     end
   end
 
-  def assert_date(str)
-    Date.parse(str)
-  end
-
-  def is_not_nil(str)
-    true unless str.nil?
+  def find_all_labels
+    [{
+         name: "Incomes",
+         applied_to: [{statement_fragment: "DIMENSION DATA"},{statement_fragment: "EXPENSES"}]
+     },
+     {
+         name: "Expenses",
+         applied_to: [{statement_fragment: "VDA"},{statement_fragment: "VDP"}]
+     },
+     {
+         name: "Vodafone",
+         applied_to: [{statement_fragment: "VODAFONE"},{statement_fragment: "VDA"}]
+     }]
   end
 
 end
